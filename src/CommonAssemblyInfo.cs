36e69042abc4d2403c558af5b9272117349a7184
LiondenLee <hsdlionden@hotmail.com>
2018-10-22 11:41:15 +0800

    +: Add application configuration file.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
 
 [assembly: AssemblyProduct("Harbour.RedisSessionStateStore")]
 
-[assembly: AssemblyCompany("http://github.com/TheCloudlessSky/Harbour.RedisSessionStateStore")]
-[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2015")]
+[assembly: AssemblyCompany("https://github.com/lionden/Harbour.RedisSessionStateStore")]
+[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2018")]
 [assembly: AssemblyDescription("An ASP.NET Redis SessionStateStoreProvider.")]
 
 [assembly: ComVisible(false)]
